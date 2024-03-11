3140332c428491f13db389be9010ef72598064c3
Monicka Akilan <monicka.rk@gmail.com>
2024-03-11 07:08:30 +0530

    contract added

---
+++ thirdweb-contracts/contracts/RealEstateContract.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8;
+
+contract RealEstatecontract {
+    function getId() public pure returns(string memory) {
+        return "Hello world";
+    }
+}
---
+++ thirdweb-contracts/hardhat.config.js
@@ -0,0 +1,42 @@
+require("@matterlabs/hardhat-zksync-solc");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = "e16582b7a2bab7c2cba20da6dad4d177a7d872a2049757dbce780cc3a26d8e6e"
+const RPC_URL = "https://rpc.ankr.com/polgon_mumbai"
+
+module.exports = {
+  defaultNetwork: "polygon_mumbai",
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  networks: {
+    hardhat : {
+      chainId: 80001,
+    },
+    polygon_mumbai: {
+      rpc_url : RPC_URL,
+      accounts: [`[0x${PRIVATE_KEY}`]
+    }
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
