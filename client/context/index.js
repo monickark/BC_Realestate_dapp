18ceec1dba4baf2b8774950b2e6423314fde6908
monickark <monicka.rk@gmail.com>
2024-03-22 07:36:11 +0530

    List Property completed

--- client/context/index.js
+++ client/context/index.js
@@ -5,14 +5,35 @@ import { ethers } from 'ethers';
 const StateContext = createContext();
 
 export const StateContextProvider = ({children}) => {
+  
     const {contract} = useContract("0x1d1062f57b65Fc578879ba4d58A7d2fC66cF3B00");
     console.log(contract);
     const address = useAddress();
     const connect = useMetamask();
     const realEstate = "Real Estate Dapp";
 
+    // FUNCTIONS
+    //1.List Property
+    const { mutateAsync: listProperty, isLoading } = useContractWrite(contract, "listProperty")
+    const createPropertyFunction = async (form) => {
+      const { owner, propTitle,  propDesc, propAddr, price, category, images} = form;
+      console.log(form);
+      console.log("owner", owner);
+      console.log("propTitle", propTitle);
+      console.log("propDesc", propDesc);
+      try {
+        const data = await listProperty({ args: [owner, price, propTitle, category, images, propAddr]});
+        console.info("contract call successs", data);
+      } catch (err) {
+        console.error("contract call failure", err);
+      }
+    }
+
+    //2.Retrieve Property    
+    const { data, isLoading1 } = useContractRead(contract, "getAllProperties", [{{args}}])
+
   return (
-    <StateContext.Provider value = {{contract, address, connect, realEstate}}>
+    <StateContext.Provider value = {{contract, address, connect, realEstate, createPropertyFunction}}>
        {children}
     </StateContext.Provider>
   );
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,14 +2,75 @@ import React, {useState, useEffect} from 'react';
 import { ethers } from 'ethers';
 
 import { useStateContext } from '../context';
+import { checkIfImage } from '@/utils';
 
 const index = () => {
-  const {contract, address, connect, realEstate} = useStateContext();
+  const {contract, address, connect, realEstate, createPropertyFunction} = useStateContext();
+  const[isLoading, setIsLoading] = useState(false)
+  const[properties, setProperties] = useState([])
+  const[form, setForm] = useState({
+    owner: "",
+    propTitle: "",
+    propDesc: "",
+    propAddr: "",
+    price: "",
+    category: "",
+    images: ""
+  });
+
+  const handleFormFieldChange = (fieldName, e) => {
+    setForm({...form, [fieldName] : e.target.value})
+  }
+
+  const handleSubmit = async(e) => {
+    alert("1. handleSubmit");
+    e.preventDefault();
+    alert("2. handleSubmit");
+    checkIfImage(form.images, async(exists) => {
+        if(exists) {
+          setIsLoading(true);
+          await createPropertyFunction({
+            ...form,
+            price: ethers.utils.parseUnits(form.price, 18),
+          });
+          setIsLoading(false);
+        } else {
+          alert("Please enter image URL.");
+          setForm(...form, images="");
+        }
+    }); 
+  }
+ 
   return  <div> 
-         <h1>{realEstate} </h1>
-       
+         <h1>{realEstate} </h1>       
          <p>{address}</p>
          <button onClick = {() => connect()}>Connect Wallet</button>
+
+         <h1>Create Property</h1> 
+         <form onSubmit = {(e) => handleSubmit(e)}>
+         <div>
+            <input type="text" placeholder='Owner Address' onChange= {(e) => handleFormFieldChange("owner", e)}/>
+            </div>
+            <div>
+            <input type="text" placeholder='PropertyTitle' onChange= {(e) => handleFormFieldChange("propTitle", e)}/>
+            </div>
+            <div>
+              <input type="text" placeholder='PropertyDescription' onChange= {(e) => handleFormFieldChange("propDesc", e)}/>
+              </div>
+            <div>
+              <input type="text" placeholder='PropertyAddr' onChange= {(e) => handleFormFieldChange("propAddr", e)}/>
+              </div>
+            <div>
+              <input type="text" placeholder='Price' onChange= {(e) => handleFormFieldChange("price", e)}/>
+              </div>
+            <div>
+              <input type="text" placeholder='category' onChange= {(e) => handleFormFieldChange("category", e)}/>
+            </div>
+            <div>
+              <input type="text" placeholder='image' onChange= {(e) => handleFormFieldChange("images", e)}/>
+            </div>
+              <button type="submit">Submit</button>
+         </form>
     </div>;
 };
 export default index;
